ca33f71333f498c6253d723bdf1ae46259dcf437
root <root@ip-172-31-8-16.ec2.internal>
2017-09-22 17:27:57 -0400

    [FIX] Ajustes para publicación en servidor nutrifami

--- nfusuarios/wsgi.py
+++ nfusuarios/wsgi.py
@@ -7,9 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+
+path = '/var/www/html/nfusuarios'
+if path not in sys.path:
+    sys.path.append(path)
+
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nfusuarios.settings")
 
