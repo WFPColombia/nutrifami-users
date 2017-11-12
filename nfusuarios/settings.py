fe555f2a8ed1fb9fc3dc8e2d01e909916e7a4b07
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2017-11-11 19:22:40 -0500

    Pruebas con google

--- nfusuarios/settings.py
+++ nfusuarios/settings.py
@@ -126,7 +126,7 @@ else:
         'DEFAULT_PERMISSION_CLASSES': (
         ),
         'DEFAULT_AUTHENTICATION_CLASSES': (
-            'rest_framework.authentication.SessionAuthentication',
+            #'rest_framework.authentication.SessionAuthentication',
         )
     }
 
